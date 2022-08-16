b7bf89c19717c182751c029aa864712674460af1
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-08-16 23:08:32 +0600

    Add files via upload
    Calculating a number whether it is Even or Odd.

---
+++ even odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int n=9;
+
+    if(n % 2 == 0){
+        printf("n is Even number\n");
+    }
+    else{
+        printf("n is Odd number\n");
+    }
+
+    return 0;
+}
