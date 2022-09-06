e2507871e44dd71fede0dc6b0769bfe77551eb8b
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-09-07 02:24:20 +0600

    Add files via upload
    Calculating the Perfect Number in "n" number range.

---
+++ perfect number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+  int n,i,sum;
+  int mn,mx;
+  printf("Input the starting range or number : ");
+  scanf("%d",&mn);
+  printf("Input the ending range of number : ");
+  scanf("%d",&mx);
+  printf("The Perfect numbers within the given range : ");
+  for(n=mn;n<=mx;n++){
+    i=1;
+    sum = 0;
+    while(i<n){
+      if(n%i==0)
+           sum=sum+i;
+          i++;
+    }
+    if(sum==n)
+      printf("%d ",n);
+  }
+
+  return 0;
+}
