c546c98403c5503cb19d62834a9766f20f6f2b69
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-09-12 00:08:32 +0600

    Add files via upload
    Calculating the sum of n numbers which are divisible by 3 and 5.

---
+++ sum of 3 and 5 divisible.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,sum=0;
+
+    for(i=30;i<=120;i++){
+        if(i%3==0 && i%5==0){
+        sum=sum+i;
+      }
+    }
+    printf("The Sum of the numbers is: %d", sum);
+
+    return 0;
+}
