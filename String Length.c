81f31b87463d781ce9d5de4debe1fd7fb3519207
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-10-17 23:07:10 +0600

    Add files via upload
    Calculating the length of a String.

---
+++ String Length.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int string_length(char str[])
+{
+    int i, length=0;
+
+    for(i=0; str[i]!='\0'; i++){
+        length++;
+    }
+    return length;
+}
+
+int main()
+{
+    int country[100];
+    int length;
+
+    while(1== scanf("%s", country)){
+          length= string_length(country);
+          printf("length: %d\n", length);
+    }
+
+    return 0;
+}
