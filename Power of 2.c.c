80340e78062a95e74b0e78f6125f0f3e9d9a4c32
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-10-27 00:45:02 +0600

    Add files via upload
    Calculating a Number in Bit wise, weather it is Power of 2 or not.

---
+++ Power of 2.c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int num;
+
+   printf("Enter the number you want to test: ");
+   scanf("%d", &num);
+
+   if((num != 0) && ((num &&(num - 1)) == 0)){
+      printf("\n%d is a power of 2", num);
+   }
+   else{
+      printf("\n%d is not a power of 2\n", num);
+   }
+
+ return 0;
+}
