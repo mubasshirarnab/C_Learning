7fca3d017852a5af46a281a4210b734af9bb7cdf
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-10-26 21:03:43 +0600

    Add files via upload

---
+++ space.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=1;
+    int b=12;
+    int c=123;
+    int d=1234;
+    int e=12345;
+
+    printf("\n%5d\n%5d\n%5d\n%5d\n%5d",a,b,c,d,e);
+
+    return 0;
+
+}
