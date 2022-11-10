f68749b73112001f13b29554ca75d3572602ec97
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-11-10 18:08:37 +0600

    Add files via upload
    *
    **
    ***
    ****
    *****

--- Number Pattern.c
+++ Number Pattern.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-   int rows, i, j, space;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
+    int i,j,n;
+    printf("Enter the number of term: ");
+    scanf("%d", &n);
 
-   for (i = rows; i >= 1; --i){
-       for (space = 0; space < rows - i; ++space){
-          printf("  ");
-       }
-       for (j = i; j <= 2 * i - 1; ++j){
-          printf("* ");
-       }
-       for (j = 0; j < i - 1; ++j){
-          printf("* ");
-       }
-       printf("\n");
-   }
+    for(i=1; i<=n; i++){
+        for(j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 
-   return 0;
+    return 0;
 }
