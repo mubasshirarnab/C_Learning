258ca4edf681fc18fb99f9a89adbc80772a87c30
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-11-21 22:29:10 +0600

    Add files via upload
    The program will take "n" positive integers into an Array A. Now find all the integers that have 
    an Odd Index and replace them by 0 in Array A. Finally show all elements of Array A.

---
+++ Odd Index Replace.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("Enter the number of term: ");
+    scanf("%d", &n);
+    int arrayA[n];
+
+    for(i=0; i<n; i++){
+        printf("Enter the %dth element: ", i+1);
+        scanf("%d", &arrayA[i]);
+    }
+    printf("\nOutput: ");
+    for(i=0; i<n; i++){
+        if(i%2==1){
+            arrayA[i]=0;
+        }
+        printf("%d,", arrayA[i]);
+    }
+
+    return 0;
+}
