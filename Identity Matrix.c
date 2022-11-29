1b7dedc78613fa7780175e5e5e0ac1c545679491
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-11-29 23:19:35 +0600

    Add files via upload
    The program will take the size of an identity matrix from the user and generate the identity 
    matrix into a 2D array.

---
+++ Identity Matrix.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of term: ");
+    scanf("%d", &n);
+    int array[n][n];
+
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            if(i==j){
+                array[i][j]=1;
+            }
+            else{
+                array[i][j]=0;
+            }
+        }
+    }
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            printf("%4d", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
