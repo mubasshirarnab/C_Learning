996a5e9535e9fb64350241e8ee405059cae0b026
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-12-25 18:28:43 +0600

    Add files via upload
    The program will remove all the duplicate elements from an Array.

---
+++ Removing Duplicates.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,temp,n;
+    printf("Enter the number of term: ");
+    scanf("%d", &n);
+    int arrayA[n];
+
+    for(i=0; i<n; i++){
+        printf("Enter the %dth element: ", i+1);
+        scanf("%d", &arrayA[i]);
+    }
+    int count=0;
+    for(i=0,j=0; i<n; i++,j++){
+        for(j=i+1; j<n; j++){
+            if(arrayA[i]==arrayA[j]){
+                count++;
+                for(int k=j; k<n; k++){
+                    arrayA[k]=arrayA[k+1];
+                }
+            }
+        }
+    }
+    printf("After removing all the duplicate elements: ");
+    for(i=0; i<count; i++){
+        printf("%d ", arrayA[i]);
+    }
+
+    return 0;
+}
