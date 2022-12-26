78e56f855a1bfdee481f0abf1d127e382f17e5df
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2022-12-26 21:58:36 +0600

    Update Removing Duplicates.c

--- Removing Duplicates.c
+++ Removing Duplicates.c
@@ -11,19 +11,19 @@ int main()
         printf("Enter the %dth element: ", i+1);
         scanf("%d", &arrayA[i]);
     }
-    int count=0;
     for(i=0,j=0; i<n; i++,j++){
         for(j=i+1; j<n; j++){
             if(arrayA[i]==arrayA[j]){
-                count++;
-                for(int k=j; k<n; k++){
+                for(int k=j; k<n-1; k++){
                     arrayA[k]=arrayA[k+1];
                 }
+                n--;
+                j--;
             }
         }
     }
     printf("After removing all the duplicate elements: ");
-    for(i=0; i<n-count; i++){
+    for(i=0; i<n; i++){
         printf("%d ", arrayA[i]);
     }
 
