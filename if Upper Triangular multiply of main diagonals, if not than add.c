82cb107d23104733e89ec2371a1b053f5000188e
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2023-01-06 01:34:47 +0600

    Add files via upload
     The program will determine whether a square matrix is an upper triangular matrix or not. If 
    the matrix is an upper triangular matrix, then multiplying all the entries of its main diagonal, 
    otherwise, adding all the entries of the main diagonal.

---
+++ if Upper Triangular multiply of main diagonals, if not than add.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,flag=0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    int array[n][n];
+
+    printf("Enter the Array Elements: \n");
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            printf("Element-[%d][%d]: ", i,j);
+            scanf("%d", &array[i][j]);
+        }
+    }
+    printf("\nArray: \n");
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            printf("%4d", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=1; i<n; i++){
+        for(j=0; j<i; j++){
+            if(array[i][j]==0){
+                flag=1;
+            }
+        }
+    }
+
+    int product=1,sum=0;
+    if(flag==1){
+        for(i=0; i<n; i++){
+            for(j=0; j<n; j++){
+                if(i==j){
+                    product=product*array[i][j];
+                }
+            }
+        }
+    }
+    else{
+        for(i=0; i<n; i++){
+            for(j=0; j<n; j++){
+                if(i==j){
+                    sum=sum+array[i][j];
+                }
+            }
+        }
+    }
+
+    if(flag==1){
+        printf("\nThe Product is: %d", product);
+    }
+    else{
+        printf("\nThe Sum is: %d", sum);
+    }
+
+    return 0;
+}
